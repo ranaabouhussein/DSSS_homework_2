df92faf0b32519e55574e28ebe9e771f6c55e807
ranaabouhussein <rana.m.abouhussein@gmail.com>
2024-11-09 13:01:17 +0100

    tree structured

---
+++ math_quiz/math_quiz/__init__.py
@@ -0,0 +1,1 @@
+from .math_quiz import generate_random_integer, get_random_operation, perform_operation
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="math_quiz",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[],  # Add required packages here
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:main',
+        ],
+    },
+    author="Rana Abouhussein",
+    description="A simple math quiz ",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/ranaabouhussein/DSSS_homework_2.git",
+    license="MIT",
+)
